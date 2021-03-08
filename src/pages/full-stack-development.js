443a2ab547636f7e2d448124e12aa9134d9e3e12
Daniel JS <daniel@mydeveloperjourney.com>
2021-03-07 19:24:13 -0600

    wip: installs genLinks utility

--- src/pages/full-stack-development.js
+++ src/pages/full-stack-development.js
@@ -1,38 +1,17 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
-import styles from '../styles/course-content-display.module.scss';
+import genLinks from '../utils/genLinks';
 
 
 export default({ data, location }) => {
-    
-   const { allMarkdownRemark } = data;
+  
 
-    const weekOneLessons = allMarkdownRemark.edges.filter(({ node }) => 
-        node.frontmatter.week === 1
-      ).map(({ node }) =>
-        <Link to={node.fields.slug}>
-          <h2 className={styles.dayTitle} key={node.fields.id}>Day {node.frontmatter.day}<small className={styles.smallText}> - {node.frontmatter.topics}</small></h2>
-        </Link> 
-      );
-      const weekTwoLessons = allMarkdownRemark.edges.filter(({ node }) => 
-      node.frontmatter.week === 2
-      ).map(({ node }) =>
-      <Link to={node.fields.slug}>
-          <h2 className={styles.dayTitle} key={node.fields.id}>Day {node.frontmatter.day}<small className={styles.smallText}> - {node.frontmatter.topics}</small></h2>
-        </Link> 
-      );
-      
-      const weekThreeLessons = allMarkdownRemark.edges.filter(({ node }) => 
-        node.frontmatter.week === 3
-      ).map(({ node }) =>
-        <Link to={node.fields.slug}>
-          <h2 className={styles.dayTitle} key={node.fields.id}>Day {node.frontmatter.day}<small className={styles.smallText}> - {node.frontmatter.topics}</small></h2>
-        </Link> 
-      );
-      
+   const { week1, week2 } = genLinks(data.allMarkdownRemark.edges);
+
+
     return (
         <Layout
           centerContent={true} 
@@ -42,17 +21,19 @@ export default({ data, location }) => {
             <h1>Full Stack Development</h1>
             <main>
               <h2>Week 1</h2>  
-              { weekOneLessons }
+              { week1 }
               <br />
               <hr />
               <br />
               <h2>Week 2</h2>  
-              { weekTwoLessons }
+              { week2}
+              {/* 
               <br />
               <hr />
               <br />
               <h2>Project Week</h2>  
-              { weekThreeLessons } 
+              { weekThreeLessons }  
+              */}
             </main>
         </Layout>
     );
