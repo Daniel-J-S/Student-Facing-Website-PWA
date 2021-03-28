22d4200d0bf4249656c746646327417aa5f2396d
Daniel JS <daniel@mydeveloperjourney.com>
2021-03-28 17:24:00 -0500

    fix: refactors to use genLinks module

--- src/pages/react-fundamentals.js
+++ src/pages/react-fundamentals.js
@@ -1,46 +1,16 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql  } from 'gatsby';
 
 import Layout from '../components/layout';
 
-import styles from '../styles/course-content-display.module.scss';
+import genLinks from '../utils/genLinks';
 
 
 export default({ data, location }) => {
     
-   const { allMarkdownRemark } = data;
+  const { week1  } = genLinks(data.allMarkdownRemark.edges);
 
-    const weekOneLessons = allMarkdownRemark.edges.filter(({ node }) => 
-        node.frontmatter.week === 1
-      ).map(({ node }) =>
-        <Link to={node.fields.slug}>
-          <h2 className={styles.dayTitle} key={node.fields.id}>{node.frontmatter.title}<small className={styles.smallText}> - {node.frontmatter.topics}</small></h2>
-        </Link> 
-      );
-            
-      const weekTwoLessons = allMarkdownRemark.edges.filter(({ node }) => 
-      node.frontmatter.week === 2
-      ).map(({ node }) =>
-        <Link to={node.fields.slug}>
-          <h2 className={styles.dayTitle} key={node.fields.id}>Day {node.frontmatter.day}<small className={styles.smallText}> - {node.frontmatter.topics}</small></h2>
-        </Link> 
-      );
-      
-      const weekThreeLessons = allMarkdownRemark.edges.filter(({ node }) => 
-        node.frontmatter.week === 3
-      ).map(({ node }) =>
-        <Link to={node.fields.slug}>
-          <h2 className={styles.dayTitle} key={node.fields.id}>Day {node.frontmatter.day}<small className={styles.smallText}> - {node.frontmatter.topics}</small></h2>
-        </Link> 
-      );
 
-      const weekFourLessons = allMarkdownRemark.edges.filter(({ node }) => 
-        node.frontmatter.week === 4
-      ).map(({ node }) =>
-        <Link to={node.fields.slug}>
-          <h2 className={styles.dayTitle} key={node.fields.id}>Day {node.frontmatter.day}<small className={styles.smallText}> - {node.frontmatter.topics}</small></h2>
-        </Link> 
-      );
     return (
         <Layout
           centerContent={true} 
@@ -50,22 +20,19 @@ export default({ data, location }) => {
             <h1>React Fundamentals</h1>
             <main>
               <h2>Week 1</h2>  
-              { weekOneLessons }
+              { week1 }
+              {/* commented out to disable
               <br />
               <hr />
               <br />
               <h2>Week 2</h2>  
-              { weekTwoLessons }
+              { week2 }
               <br />
               <hr />
               <br />
               <h2>Project Week</h2>  
-              { weekThreeLessons }
-              <br />
-              <hr />
-              <br />
-              <h2>Project Week & Presentations</h2>  
-              { weekFourLessons }
+              { week3 }
+              */}
             </main>
         </Layout>
     );
