b4f6a8d4f2772b54190d267434514aaa82bcc9e3
Daniel JS <daniel@mydeveloperjourney.com>
2021-08-25 10:55:54 -0500

    wip: adds support for global layout mounting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+// TODO: Add more efficient loading of CSS
+// const React = require("react")
+// const Layout = require("./src/components/layout")
+
 export const onServiceWorkerUpdateReady = () => {
     const answer = window.confirm(
       `This application has been updated. ` +
@@ -8,3 +12,15 @@ export const onServiceWorkerUpdateReady = () => {
       window.location.reload()
     }
   }
+
+/*
+// Logs when the client route changes
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
+// Wraps every page in a component
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
+*/
