43a80ac92820476ba2d93c45f27d129a6d8f5330
Daniel JS <daniel@mydeveloperjourney.com>
2021-09-10 07:51:44 -0500

    wip: unlocks next weeks content

--- src/pages/frontend-fundamentals.js
+++ src/pages/frontend-fundamentals.js
@@ -8,7 +8,7 @@ import { Breadcrumb } from 'gatsby-plugin-breadcrumb';
 
 const FrontendFundamentals = ({ data, location }) => {
 
-   const { week1 } = genLinks(data.allMarkdownRemark.edges);
+   const { week1, week2 } = genLinks(data.allMarkdownRemark.edges);
        
     return (
         <>
@@ -25,12 +25,12 @@ const FrontendFundamentals = ({ data, location }) => {
             <main>
               <h2>Week 1</h2>  
               { week1 }
-              {/* 
               <br />
               <hr />
               <br />
               <h2>Week 2</h2>  
               { week2 }
+              {/* 
               <br />
               <hr />
               <br />
